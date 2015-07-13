68711488e38c6a3e6b11fb319136ce7d0429aa29
Max Rosin <dynstatus@hackrid.de>
2015-07-13 21:48:23 +0200

    removed global variables

--- location.py
+++ location.py
@@ -3,31 +3,27 @@ import network
 
 DEFAULT_POINTS = 10
 
-__ssid_of_accesspoint = network.get_ssid_of_accesspoint()
-__mac_of_accesspoint = network.get_mac_of_accesspoint()
-__connected_interfaces = network.get_connected_interfaces_and_ip()
-
-def __check_ssid(location):
+def __check_ssid(location,ssid):
     if 'ssid' in location:
-        if __ssid_of_accesspoint in location['ssid']:
+        if ssid in location['ssid']:
             location['__score'] += DEFAULT_POINTS
             logging.debug('+SSID ==> {}'.format(location['__score']))
         else:
             location['__score'] = -1000
             logging.debug('-SSID ==> {}'.format(location['__score']))
 
-def __check_mac(location):
+def __check_mac(location,mac):
     if 'mac' in location:
-        if __mac_of_accesspoint in location['mac']:
+        if mac in location['mac']:
             location['__score'] += DEFAULT_POINTS
             logging.debug('+MAC ==> {}'.format(location['__score']))
         else:
             location['__score'] = -1000
             logging.debug('-MAC ==> {}'.format(location['__score']))
 
-def __check_ip(location):
+def __check_ip(location,interfaces):
     if 'ip' in location:
-        if __connected_interfaces[location['interface']] in location['ip']:
+        if interfaces[location['interface']] in location['ip']:
             location['__score'] += DEFAULT_POINTS
             logging.debug('+IP ==> {}'.format(location['__score']))
         else:
@@ -36,17 +32,22 @@ def __check_ip(location):
 
 
 def get_matching_config(config):
+
+    ssid = network.get_ssid_of_accesspoint()
+    mac = network.get_mac_of_accesspoint()
+    interfaces = network.get_connected_interfaces_and_ip()
+
     best_config = config.dynstatus['default']
     best_config['__score'] = 0
 
     for location in config.dynstatus['locations']:
         logging.debug('='*45)
         logging.debug('Checking: {}'.format(location['name']))
-        if location['interface'] in __connected_interfaces:
+        if location['interface'] in interfaces:
             location['__score'] = 0
-            __check_ssid(location)
-            __check_mac(location)
-            __check_ip(location)
+            __check_ssid(location,ssid)
+            __check_mac(location,mac)
+            __check_ip(location,interfaces)
 
             if location['__score'] > best_config['__score']:
                 best_config = location
