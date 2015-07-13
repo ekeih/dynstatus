42c9b8ce5e3e18f16f1b447d16bb26ee933234de
Max Rosin <dynstatus@hackrid.de>
2015-07-13 21:48:50 +0200

    logging in network module

--- network.py
+++ network.py
@@ -1,13 +1,16 @@
+import logging
 from subprocess import Popen, PIPE
 
 def get_mac_of_accesspoint():
     command = Popen('iwgetid -ra', shell=True, stdout=PIPE)
     mac = command.stdout.read().decode('utf-8')[:-1]
+    logging.debug('MAC of access point: {}'.format(mac))
     return mac
 
 def get_ssid_of_accesspoint():
     command = Popen('iwgetid -r', shell=True, stdout=PIPE)
     ssid = command.stdout.read().decode('utf-8')[:-1]
+    logging.debug('SSID of access point: {}'.format(ssid))
     return ssid
 
 def get_connected_interfaces_and_ip():
@@ -16,5 +19,6 @@ def get_connected_interfaces_and_ip():
     interface_ip_pairs = {}
     for pair in list_of_interfaces_and_ips:
         ip,interface = pair.split()
+        logging.debug('Iface: {}, IP: {}'.format(interface,ip))
         interface_ip_pairs[interface] = ip
     return interface_ip_pairs
