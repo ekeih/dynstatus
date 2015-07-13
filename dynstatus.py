100ab8c72a614c688d34561c3f7464c54f207772
Max Rosin <dynstatus@hackrid.de>
2015-07-13 21:01:51 +0200

    improved logging and argument parsing

--- dynstatus.py
+++ dynstatus.py
@@ -1,23 +1,23 @@
 #!/usr/bin/env python3
 
+import logging
 import os
 import sys
 import glob
 import location
-import logging
 import argparse
 
 def setup_configuration():
-    logging.debug('setup configuration')
     config_path = os.environ.get('XDG_CONFIG_HOME')
     home_path = os.environ.get('HOME')
 
     if config_path != None:
         config_path = config_path + '/dynstatus'
     elif home_path != None:
+        logging.debug('$XDG_CONFIG_HOME undefined. Fallback to $HOME/.config/')
         config_path = home_path + '/.config/dynstatus'
 
-    logging.info('config_path %s', config_path)
+    logging.debug('config_path {}'.format(config_path))
     if not (os.access(config_path + '/config.py', os.F_OK)):
         logging.error('Config file not found')
         exit()
@@ -26,11 +26,9 @@ def setup_configuration():
         global config
         import config
 
-
 def run_plugins():
     logging.debug('run_plugins')
     best_location = location.get_matching_config(config)
-    logging.info('Best location: ' + best_location['name'])
     plugins_folder = os.path.expanduser(config.dynstatus['plugins_path'])
     logging.debug('Plugins path: ' + plugins_folder)
     sys.path.append(plugins_folder)
@@ -45,11 +43,11 @@ def run_daemon():
     import time
     logging.info('Running dynstatus in daemon mode.')
     interval = config.dynstatus['daemon_interval']
-    logging.info('Interval: %s', interval)
+    logging.debug('daemon: interval: {}'.format(interval))
     if os.fork():
         sys.exit()
     while True:
-        logging.info('Run plugins')
+        logging.debug('daemon: run plugins')
         run_plugins()
         time.sleep(interval)
 
@@ -59,28 +57,59 @@ def print_help():
 
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='Dynstatus tries to pinpoint your location based on your network configuration (WLAN, IP, ...) and enables you to run code to react to changes.')
-    parser.add_argument('-d','--daemon',action="store_true",dest="daemon",help='Dynstatus will fork to background and detect your location and run your plugins periodically. The interval is configured in the config.py of dynstatus.',required=False,default=False)
-    parser.add_argument('-g','--get', help='Dynstatus can give you status information without running the plugins. You can ask for every location setting of your config.py.',default='ssid',required=False)
+    parser = argparse.ArgumentParser(description='Dynstatus tries to pinpoint \
+        your location based on your network configuration (WLAN, IP, ...) and \
+        enables you to run code to react to changes.')
+    parser.add_argument(
+        '-d',
+        '--daemon',
+        action='store_true',
+        dest='daemon',
+        help='Dynstatus will fork to background and detect your location and \
+            run your plugins periodically. The interval is configured in the \
+            config.py of dynstatus.',
+        default=False,
+        required=False)
+    parser.add_argument(
+        '-g',
+        '--get',
+        help='Dynstatus can give you status information without running the \
+            plugins. You can ask for every location setting of your config.py.',
+        required=False)
+    parser.add_argument(
+        '-v',
+        '--verbose',
+        help='Enable verbose output.',
+        action='store_true',
+        default=False,
+        required=False)
     args = parser.parse_args()
 
-    logging.basicConfig(format='%(asctime)s: (%(levelname)s) %(message)s', datefmt='%d.%m.%Y %H:%M:%S', level=logging.DEBUG)
-    logging.debug('=============================================')
+    loglevel = 'INFO'
+    if args.verbose:
+        loglevel = 'DEBUG'
+
+    logging.basicConfig(
+        format='%(asctime)s (%(levelname)s) %(message)s',
+        datefmt='%H:%M:%S', level=loglevel)
+    #logging.basicConfig(filename='log.txt', level=loglevel)
+    logging.debug('='*45)
     logging.debug('dynstatus v0.1')
     logging.debug('Developed by Max Rosin <dynstatus@hackrid.de>')
-    logging.debug('=============================================')
+    logging.debug('='*45)
     setup_configuration()
 
     # argparse
-    if len(sys.argv) <= 1:
+    if not (args.daemon or args.get):
         run_plugins()
     elif args.daemon:
         run_daemon()
     elif args.get:
         best_location = location.get_matching_config(config)
         if args.get in best_location:
+            logging.debug('{}: {}'.format(args.get, best_location[args.get]))
             print(best_location[args.get])
         else:
-            print('Unknown value.')
+            logging.error('Unknown value: {}'.format(args.get))
     else:
         print_help()
--- location.py
+++ location.py
@@ -1,3 +1,4 @@
+import logging
 import network
 
 DEFAULT_POINTS = 10
@@ -10,22 +11,28 @@ def __check_ssid(location):
     if 'ssid' in location:
         if __ssid_of_accesspoint in location['ssid']:
             location['__score'] += DEFAULT_POINTS
+            logging.debug('+SSID ==> {}'.format(location['__score']))
         else:
             location['__score'] = -1000
+            logging.debug('-SSID ==> {}'.format(location['__score']))
 
 def __check_mac(location):
     if 'mac' in location:
         if __mac_of_accesspoint in location['mac']:
             location['__score'] += DEFAULT_POINTS
+            logging.debug('+MAC ==> {}'.format(location['__score']))
         else:
             location['__score'] = -1000
+            logging.debug('-MAC ==> {}'.format(location['__score']))
 
 def __check_ip(location):
     if 'ip' in location:
         if __connected_interfaces[location['interface']] in location['ip']:
             location['__score'] += DEFAULT_POINTS
+            logging.debug('+IP ==> {}'.format(location['__score']))
         else:
             location['__score'] = -1000
+            logging.debug('-IP ==> {}'.format(location['__score']))
 
 
 def get_matching_config(config):
@@ -33,6 +40,8 @@ def get_matching_config(config):
     best_config['__score'] = 0
 
     for location in config.dynstatus['locations']:
+        logging.debug('='*45)
+        logging.debug('Checking: {}'.format(location['name']))
         if location['interface'] in __connected_interfaces:
             location['__score'] = 0
             __check_ssid(location)
@@ -41,5 +50,8 @@ def get_matching_config(config):
 
             if location['__score'] > best_config['__score']:
                 best_config = location
-
+        else:
+            logging.debug('Interface not connected.')
+    logging.debug('='*45)
+    logging.info('Best location: {}'.format(best_config['name']))
     return best_config
